235c09575fb23cd74455f48391333515293940dd
aeremin <aeremin@scnsoft.com>
2023-02-17 17:20:51 +0300

    Updates

---
+++ assets/Input-ec5c5151.js
@@ -0,0 +1,1 @@
+import{j as n,c as o}from"./index-3105ded0.js";const p="_input_9lv91_1",u={input:p},i=({cssClass:t,...s})=>n.exports.jsx("input",{className:o(u.input,t),autoComplete:"off",...s});export{i as I};
